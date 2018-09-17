3178c146d36ca505bee37f234671e554d5d869a9
DTPelopones <pelopones@mail.ru>
2018-09-17 22:25:33 +0300

    Add Afisha

---
+++ MoscowCamerat/Controllers/SoloistsController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace MoscowCamerat.Controllers
+{
+    public class SoloistsController : BaseController
+    {
+        // GET: Soloist
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
